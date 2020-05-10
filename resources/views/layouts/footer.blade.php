2cec7ca36a7fd703902c6f0e6fb11d33ab8ea499
Prawee Wongsa <prawee@hotmail.com>
2020-05-10 21:48:49 +0700

    create main,navbar,footer of layout and using it to default

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link http://www.konkeanweb.com
+ * 2020-05-10 21:10
+ * @copyright Copyright (c) 2020 served
+ * @author Prawee Wongsa <prawee@hotmail.com>
+ * @license MIT
+ */
+?>
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+    <!-- Footer Links -->
+
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Learning:
+        <a href="https://konkeanweb.com/"> konkeanweb.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
