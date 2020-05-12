88bb849ab84ce5beb0abec8195d9a9f51fb4377d
Prawee Wongsa <prawee@hotmail.com>
2020-05-12 15:02:49 +0700

    update security of change password with middleware

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('auth.passwords.change');
